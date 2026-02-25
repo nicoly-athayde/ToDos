420df2572c717bb7e04b4fb9ece3a676c8788b34
nicoly-athayde <nicolyathayde4@gmail.com>
2026-02-25 12:26:51 -0300

    Continuação

---
+++ ToDoPlatform/Models/AppUser.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace ToDoPlatform.Models;
+
+public class AppUser : IdentityUser
+{
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    [StringLength(300)]
+    public string ProfilePicture { get; set; }
+}
